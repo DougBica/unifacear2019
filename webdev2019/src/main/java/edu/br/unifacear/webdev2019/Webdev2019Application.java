ab5852ad7d6a173e930c0221bdc015a064c779a9
CLEVERSON AVELINO Ferreira <cleversonavelino@gmail.com>
2019-08-02 20:08:34 -0300

    inclusao do projeto

---
+++ webdev2019/src/main/java/edu/br/unifacear/webdev2019/Webdev2019Application.java
@@ -0,0 +1,13 @@
+package edu.br.unifacear.webdev2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webdev2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webdev2019Application.class, args);
+	}
+
+}
