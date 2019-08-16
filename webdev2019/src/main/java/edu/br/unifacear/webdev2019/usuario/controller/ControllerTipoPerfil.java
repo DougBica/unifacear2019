2e2a5f8c4010167431fa5894e415f2d39cfb77c3
Senger <gabriel-senger@hotmail.com>
2019-08-16 20:32:23 -0300

    changes

---
+++ webdev2019/src/main/java/edu/br/unifacear/webdev2019/usuario/controller/ControllerTipoPerfil.java
@@ -0,0 +1,30 @@
+package edu.br.unifacear.webdev2019.usuario.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.br.unifacear.webdev2019.usuario.entity.TipoPerfil;
+
+@RestController
+@RequestMapping("/tipoperfil")
+public class ControllerTipoPerfil {
+	
+//	@Autowired
+//	private TipoPerfilService tipoperfilService;
+	
+//	@PostMapping
+//	public void salvar(@RequestBody TipoPerfil tipoperfil) {
+//		tipoperfilService.salvar(tipoperfil);
+//	}
+//	
+//	@GetMapping
+//	public List<TipoPerfil> listar() {
+//		return tipoperfilService.listar();
+//	}
+//
+}
