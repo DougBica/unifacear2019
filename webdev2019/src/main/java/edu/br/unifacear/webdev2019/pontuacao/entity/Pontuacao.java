081fa6a5e538a7235653bae2b33fd12f29b5d5ab
Blasius <dyousb@gmail.com>
2019-08-15 21:13:10 -0300

    inclusao da classe pontuacao

--- webdev2019/src/main/java/edu/br/unifacear/webdev2019/pontuacao/entity/Pontuacao.java
+++ webdev2019/src/main/java/edu/br/unifacear/webdev2019/pontuacao/entity/Pontuacao.java
@@ -1,5 +1,24 @@
 package edu.br.unifacear.webdev2019.pontuacao.entity;
 
-public class Pontuacao {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Pontuacao")
+public class Pontuacao {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long guidPontuacao;
+	
+	
+	public Long getGuidPontuacao() {
+		return guidPontuacao;
+	}
+
+	public void setGuidPontuacao(Long guidPontuacao) {
+		this.guidPontuacao = guidPontuacao;
+	}
 }
