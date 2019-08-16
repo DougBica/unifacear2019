8c3ca9f6911f7a87408c11f0e44f316270b35e41
Senger <gabriel-senger@hotmail.com>
2019-08-16 19:44:46 -0300

    changes

--- webdev2019/src/main/java/edu/br/unifacear/webdev2019/usuario/entity/Usuario.java
+++ webdev2019/src/main/java/edu/br/unifacear/webdev2019/usuario/entity/Usuario.java
@@ -1,10 +1,23 @@
 package edu.br.unifacear.webdev2019.usuario.entity;
 
+import java.util.List;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @Entity
 @Table(name = "USUARIO")
@@ -15,6 +28,22 @@ public class Usuario {
 	private Long guidUsuario;
 	private String nome;
 	private String login;
+	
+	@ElementCollection
+    @CollectionTable(name = "TIPO_USUARIOS",joinColumns = @JoinColumn(name = "guidUsuario") ,  foreignKey=@ForeignKey(name = "FK_USUARIO_ROLES"))
+    @Column(name = "ROLE")
+    @Enumerated(EnumType.STRING)
+	@Fetch(FetchMode.SUBSELECT)
+	private List<TipoUsuario> TipoDeUsuarios;
+	
+	
+	public List<TipoUsuario> getTipoDeUsuarios() {
+		return TipoDeUsuarios;
+	}
+
+	public void setTipoDeUsuarios(List<TipoUsuario> tipoDeUsuarios) {
+		TipoDeUsuarios = tipoDeUsuarios;
+	}
 
 	public Long getGuidUsuario() {
 		return guidUsuario;
