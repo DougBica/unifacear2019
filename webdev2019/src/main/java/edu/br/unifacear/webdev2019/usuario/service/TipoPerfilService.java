6b54859634a5c9a917a034771dd87f959a2fd5a3
unknown <leo.sn159@gmail.com>
2019-08-16 20:34:01 -0300

    changes

---
+++ webdev2019/src/main/java/edu/br/unifacear/webdev2019/usuario/service/TipoPerfilService.java
@@ -0,0 +1,33 @@
+package edu.br.unifacear.webdev2019.usuario.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.br.unifacear.webdev2019.usuario.entity.TipoPerfil;
+import edu.br.unifacear.webdev2019.usuario.entity.Usuario;
+import edu.br.unifacear.webdev2019.usuario.repository.TipoPerfilRepository;
+import edu.br.unifacear.webdev2019.usuario.repository.UsuarioRepository;
+
+@Service
+public class TipoPerfilService {
+
+	@Autowired
+	private TipoPerfilRepository tipoPerfilRepository;
+	
+	@Transactional
+	public void salvar(TipoPerfil tipoPerfil) {		
+		tipoPerfilRepository.save(tipoPerfil);
+	}	
+	
+	public void excluir(TipoPerfil tipoPerfil) {
+		tipoPerfilRepository.delete(tipoPerfil);
+	}
+	
+	public List<TipoPerfil> listar() {
+		return tipoPerfilRepository.findAll();
+	}
+	
+}
