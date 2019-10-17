b0a5be99bff288f46a7c26fc792bfa33eb02674c
Cleverson Avelino Ferreira <cleversonavelino@gmail.com>
2019-10-17 20:57:19 -0300

    jwt

--- frontend/src/app/pages/usuario/usuario.service.ts
+++ frontend/src/app/pages/usuario/usuario.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Usuario } from './model/usuario.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,11 @@ export class UsuarioService {
   constructor(private http : HttpClient) { }
 
   list() : Observable<Usuario[]> {
-    return this.http.get<Usuario[]>("http://localhost:8080/usuario/listar");
+    let options = {
+      headers: new HttpHeaders().set('Authorization', "Bearer "+localStorage.getItem('token'))
+    };
+    
+    return this.http.get<Usuario[]>("http://localhost:8080/scp/private/usuario",options);
   }
 
   buscarPorID(guidUsuario: string) : Observable<Usuario>  {
@@ -21,6 +25,17 @@ export class UsuarioService {
   salvar(usuario: Usuario) : Observable<any> {
     return this.http.post<any>
       ("http://localhost:8080/usuario/salvar/",usuario);
+  }
+
+  login(login: string, senha: string) : Observable<any> {
+    let options = {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    };
+
+    return this.http.post<any>
+      ("http://localhost:8080/scp/public/login",
+        "login="+login+"&senha="+senha, 
+        options);
   }
 
 }
