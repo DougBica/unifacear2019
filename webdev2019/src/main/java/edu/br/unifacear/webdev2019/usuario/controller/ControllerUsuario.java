88528a39129586e82e0720b44510389fd948dbf9
Cleverson Avelino Ferreira <cleversonavelino@gmail.com>
2019-10-17 19:25:48 -0300

    including users fake

--- webdev2019/src/main/java/edu/br/unifacear/webdev2019/usuario/controller/ControllerUsuario.java
+++ webdev2019/src/main/java/edu/br/unifacear/webdev2019/usuario/controller/ControllerUsuario.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,8 +32,9 @@ public class ControllerUsuario {
 		return usuarioService.listar();
 	}
 
-	@GetMapping("/BuscarPorID")
-	public Usuario BuscarPorID(final Long guidUsuario) {
+	@GetMapping("/buscarPorID/{guidUsuario}")
+	public Usuario BuscarPorID(@PathVariable("guidUsuario")
+		final Long guidUsuario) {
 		return usuarioService.buscarPorId(guidUsuario);
 	}
 	
