9642eb9ab7ce8d9f4d6789f7b2f75f9d8e374325
GabrielC10 <gabrielc10@github.com>
2019-10-18 19:54:04 -0300

    bug fix

--- frontend/src/app/pages/usuario/usuario.module.ts
+++ frontend/src/app/pages/usuario/usuario.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsuarioListarComponent } from './usuario-listar/usuario-listar.component';
 import { UsuarioCadastrarComponent } from './usuario-cadastrar/usuario-cadastrar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [UsuarioListarComponent, UsuarioCadastrarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class UsuarioModule { }
