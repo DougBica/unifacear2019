21e970e656d2c90a87508093dbd6da3c4f340549
MateusSuzena <mateus.suzena@alta-rt.com>
2019-10-26 16:10:22 -0300

    Adicionado modulo passagem ao frontend

---
+++ frontend/src/app/pages/passagem/carrinho-passagem/carrinho-passagem.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho-passagem',
+  templateUrl: './carrinho-passagem.component.html',
+  styleUrls: ['./carrinho-passagem.component.scss']
+})
+export class CarrinhoPassagemComponent implements OnInit {
+
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ frontend/src/app/pages/passagem/passagem.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarrinhoPassagemComponent } from './carrinho-passagem/carrinho-passagem.component';
+
+@NgModule({
+  declarations: [CarrinhoPassagemComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PassagemModule { }
