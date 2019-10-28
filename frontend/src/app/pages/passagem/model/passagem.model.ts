ef6f338e2ff0c0b9c8608df852d4fd123e533307
MateusSuzena <mateus.suzena@alta-rt.com>
2019-10-28 12:36:09 -0300

    Corrigdo Model Passagem no frontend. Adicionado os atributos origem e destino

--- frontend/src/app/pages/passagem/model/passagem.model.ts
+++ frontend/src/app/pages/passagem/model/passagem.model.ts
@@ -4,10 +4,12 @@ export class Passagem {
     guidRota: number;
     classePassagem:string;
     dataPartida: Date;
+    origem: string;
+    destino: string;
     active: boolean;
     valorPassagem: number;
     nomePassageiro: string;
     cpfPassageiro: string;
     // CRIAR OBJETO RESERVA AQUI
-    
+
 }
