4422df58d8bdfac8520cc761b1badc5d9fd95789
MateusSuzena <mateus.suzena@alta-rt.com>
2019-10-28 12:25:33 -0300

    Criado model da passagem. Ainda falta criar o objeto Reserva dentro.

---
+++ frontend/src/app/pages/passagem/model/passagem.model.ts
@@ -0,0 +1,13 @@
+export class Passagem {
+
+    guidPassagem: number;
+    guidRota: number;
+    classePassagem:string;
+    dataPartida: Date;
+    active: boolean;
+    valorPassagem: number;
+    nomePassageiro: string;
+    cpfPassageiro: string;
+    // CRIAR OBJETO RESERVA AQUI
+    
+}
