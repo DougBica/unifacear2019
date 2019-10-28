09fb7ac285d641297ac7010f4a092c358e7f8a14
MateusSuzena <mateus.suzena@alta-rt.com>
2019-10-28 19:34:05 -0300

    Modifciado table das passagens para ser criado de forma dinamica partir de um array de passagens que será recebido da tela buscar-passagem

--- frontend/src/app/pages/passagem/carrinho-passagem/carrinho-passagem.component.ts
+++ frontend/src/app/pages/passagem/carrinho-passagem/carrinho-passagem.component.ts
@@ -15,6 +15,50 @@ export class CarrinhoPassagemComponent implements OnInit {
   reserva: Reserva;
   tipoPagamento: TipoPagamento;
   data: Date = new Date('1995-12-17T03:24:00');
+
+  // Este array será populado com o array que virá da tela buscar-passagem
+  listaPassagens: Array<Passagem> = [
+    { 
+      guidPassagem: 1,
+      guidRota: 1,
+      classePassagem: "CLASSE_EXECUTIVA",
+      dataPartida: new Date(),
+      origem: "Curti",
+      destino: "sao",
+      active: true,
+      valorPassagem: 2,
+      nomePassageiro: "ADSas",
+      cpfPassageiro: "89",  
+      reserva: new Reserva()
+    },
+    { 
+      guidPassagem: 2,
+      guidRota: 1,
+      classePassagem: "CLASSE_EXECUTIVA",
+      dataPartida: new Date(),
+      origem: "origem2",
+      destino: "destino2",
+      active: true,
+      valorPassagem: 2,
+      nomePassageiro: "ADSas",
+      cpfPassageiro: "89",  
+      reserva: new Reserva()
+    },
+    { 
+      guidPassagem: 3,
+      guidRota: 1,
+      classePassagem: "CLASSE_EXECUTIVA",
+      dataPartida: new Date(),
+      origem: "origem3",
+      destino: "destino3",
+      active: true,
+      valorPassagem: 2,
+      nomePassageiro: "ADSas",
+      cpfPassageiro: "89",  
+      reserva: new Reserva()
+    }
+  ]
+
   
   constructor(private passagemService: PassagemService) { }
 
