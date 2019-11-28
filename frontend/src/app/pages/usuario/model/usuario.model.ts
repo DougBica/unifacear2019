7e2165300ff9ac6710539bed333f7e32cb0938e9
unknown <leo.sn159@gmail.com>
2019-11-28 20:43:25 -0300

    att

--- frontend/src/app/pages/usuario/model/usuario.model.ts
+++ frontend/src/app/pages/usuario/model/usuario.model.ts
@@ -7,12 +7,8 @@ export class Usuario {
     email:string;
     dataNasc: any;
     cpf : String;
-<<<<<<< HEAD
-    foto : String;
     tipoDeUsuario : String;
-=======
     foto : number;
->>>>>>> 3fb853e57e8d24d66f2d98942f49da21d0fc25c2
     perfil: Perfil;
     perfis : Perfil [] = [];
 
