2dd3a8206dd7b6bac69792a2c89eaa95844aa09e
Richard Prandt <rprandt.contato@gmail.com>
2019-12-10 11:29:29 -0200

    crossorigin added

--- webdev2019/src/main/java/edu/br/unifacear/webdev2019/remarcacao/controller/RemarcaController.java
+++ webdev2019/src/main/java/edu/br/unifacear/webdev2019/remarcacao/controller/RemarcaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import edu.br.unifacear.webdev2019.remarcacao.service.RemarcaService;
 
 @RestController
 @RequestMapping("/scp/private/remarca")
+@CrossOrigin("*")
 public class RemarcaController {
 		
 		@Autowired
